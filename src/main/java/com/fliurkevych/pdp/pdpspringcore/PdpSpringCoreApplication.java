c08c9e820e971dd75db9f2a584d645043d8d56b2
Oleh Fliurkevych <oleh0809@gmail.com>
2022-09-03 11:19:44 +0300

    Initial commit

---
+++ src/main/java/com/fliurkevych/pdp/pdpspringcore/PdpSpringCoreApplication.java
@@ -0,0 +1,13 @@
+package com.fliurkevych.pdp.pdpspringcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdpSpringCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdpSpringCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fliurkevych/pdp/pdpspringcore/PdpSpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fliurkevych.pdp.pdpspringcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdpSpringCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
