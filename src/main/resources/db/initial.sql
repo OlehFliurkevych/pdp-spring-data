1f0e9b67c931e9474076be18c6e21c2842dd9843
Oleh Fliurkevych <oleh0809@gmail.com>
2022-11-29 00:16:21 +0200

    Generate DDL script

---
+++ src/main/resources/db/initial.sql
@@ -0,0 +1,44 @@
+-- pdp_spring.event definition
+
+create TABLE `event` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `date` date DEFAULT NULL,
+  `ticket_price` decimal(19,2) DEFAULT NULL,
+  `title` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- pdp_spring.user_account definition
+
+create TABLE `user_account` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `balance` decimal(19,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- pdp_spring.`user` definition
+
+create TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `email` varchar(60) DEFAULT NULL,
+  `name` varchar(60) DEFAULT NULL,
+  `user_account_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKa2ixh18irxw16xxl1ka3gfth6` (`user_account_id`),
+  CONSTRAINT `FKa2ixh18irxw16xxl1ka3gfth6` FOREIGN KEY (`user_account_id`) REFERENCES `user_account` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- pdp_spring.ticket definition
+
+create TABLE `ticket` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `category` varchar(255) DEFAULT NULL,
+  `place` bigint(20) DEFAULT NULL,
+  `event_id` bigint(20) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKfytuhjopeamxbt1cpudy92x5n` (`event_id`),
+  KEY `FKdvt57mcco3ogsosi97odw563o` (`user_id`),
+  CONSTRAINT `FKdvt57mcco3ogsosi97odw563o` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `FKfytuhjopeamxbt1cpudy92x5n` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
